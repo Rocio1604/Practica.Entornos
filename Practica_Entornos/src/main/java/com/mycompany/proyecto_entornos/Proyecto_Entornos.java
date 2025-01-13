25e13eb4a6bd6864c2466aa16945ee94cde1c925
Rocio1604 <rocpago@gmail.com>
2025-01-13 10:50:57 +0100

    Add files via upload

---
+++ Practica_Entornos/src/main/java/com/mycompany/proyecto_entornos/Proyecto_Entornos.java
@@ -0,0 +1,12 @@
+package com.mycompany.proyecto_entornos;
+
+/**
+ *
+ * @author Lu Piraquive
+ */
+public class Proyecto_Entornos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
