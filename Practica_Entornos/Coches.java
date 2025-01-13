1a86c34674b13f47b9501daab3d16f9ba7777b48
AndreaPiraquive <andrea.piraquive@juanxxiii.net>
2025-01-13 12:33:46 +0100

    Add files via upload
    Agrego una clase coche

---
+++ Practica_Entornos/Coches.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto_entornos.modelo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lu Piraquive
+ */
+public class Coches {
+    
+ Scanner Teclado = new Scanner(System.in);
+ 
+}
